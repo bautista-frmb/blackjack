255a3e968d8e9eeed02dd504332394ad70182e8c
bautista-frmb <54732436+bautista-frmb@users.noreply.github.com>
2023-03-02 16:59:52 +0800

    Delete Python directory

--- Python/blackjack/__init__.py
+++
@@ -1,5 +0,0 @@
-from .card import Card
-from .hand import Hand
-from .deck import Deck
-
-__all__ = [Card, Deck, Hand]
--- Python/blackjack/card.py
+++
@@ -1,8 +0,0 @@
-class Card:
-
-    def __init__(self, suit: str, rank: str):
-        self.suit = suit
-        self.rank = rank
-
-    def __str__(self) -> str:
-        return f'{self.rank}{self.suit}'
